4ede836d9db54915e6b1b5c81bf54bd472022052
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-06-27 12:09:44 +0530

    Create UserEntity.java

---
+++ Module-2/SLF4J Logging exercises/Exercise-2/UserEntity.java
@@ -0,0 +1,23 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String name;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
