7e93f797ac268036e5508ef16cc99964f9cd7caa
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-06-27 12:12:44 +0530

    Create UserController.java

---
+++ Module-2/SLF4J Logging exercises/Exercise-3: Testing a REST Controller with MockMvc/UserController.java
@@ -0,0 +1,12 @@
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+}
