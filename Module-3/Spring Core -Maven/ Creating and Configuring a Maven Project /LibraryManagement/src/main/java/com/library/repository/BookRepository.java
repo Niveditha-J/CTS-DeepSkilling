9e003b70c8448fa0c6c4fae01ad9e739b8e6b9b4
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-07-06 12:54:10 +0530

    Create BookRepository.java

---
+++ Module-3/Spring Core -Maven/ Creating and Configuring a Maven Project /LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+
+package com.library.repository;
+
+public class BookRepository {
+    public void getAllBooks() {
+        System.out.println("Book list: [‘Java Basics’, ‘Spring Framework’, ‘Data Structures’]");
+    }
+}
