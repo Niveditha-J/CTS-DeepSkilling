f459aa0d574555aa80071e96c9f1cc20978833b7
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-07-06 12:59:30 +0530

    Create BookRepository.java

---
+++ Module-3/Spring Core -Maven/Configuring the Spring IoC Container/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void getAllBooks() {
+        System.out.println("BookRepository: List of books: [Java Basics, Spring Framework, Data Structures]");
+    }
+}
