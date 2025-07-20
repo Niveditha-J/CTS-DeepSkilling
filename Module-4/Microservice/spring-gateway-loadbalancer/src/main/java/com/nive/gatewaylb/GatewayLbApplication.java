b05723d90957c26fcefb83124d1db33dd061a408
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-07-20 08:36:45 +0530

    Create GatewayLbApplication.java

---
+++ Module-4/Microservice/spring-gateway-loadbalancer/src/main/java/com/nive/gatewaylb/GatewayLbApplication.java
@@ -0,0 +1,11 @@
+package com.nive.gatewaylb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayLbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayLbApplication.class, args);
+    }
+}
