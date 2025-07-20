96515b3e81832588e34ee9dc3d13997cc8bb1db2
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-07-20 08:16:29 +0530

    Create JwtTokenProvider.java

---
+++ Module-4/Microservice/spring-jwt-secure-api/src/main/java/com/nive/jwtapi/security/JwtTokenProvider.java
@@ -0,0 +1,49 @@
+package com.nive.jwtapi.security;
+
+import com.nive.jwtapi.config.JwtConfig;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    public String createToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 3600000); // 1 hour
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret())
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = Jwts.parser()
+                .setSigningKey(jwtConfig.getSecret())
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        return new UsernamePasswordAuthenticationToken(username, "", null);
+    }
+}
