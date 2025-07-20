0155098912bceeea2fbe201059a9ecf51ca53ce8
Niveditha-J <115526325+Niveditha-J@users.noreply.github.com>
2025-07-20 07:58:48 +0530

    Create Oauth2AuthApplication.java

---
+++ Module-4/Microservice/spring-oauth2-auth/src/main/java/com/nive/oauth2auth/Oauth2AuthApplication.java
@@ -0,0 +1,12 @@
+package com.nive.oauth2auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2AuthApplication.class, args);
+    }
+}
